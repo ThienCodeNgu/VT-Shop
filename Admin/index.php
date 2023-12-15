4c1f10df58c4d51fd00d1b73cdf4c7d8d32e1bab
ThienCodeNgu <thiencuaai123@gmail.com>
2023-12-15 12:59:02 +0700

    a

--- Admin/index.php
+++ Admin/index.php
@@ -1,13 +1,39 @@
-<?php 
+<?php
 session_start();
 ob_start();
-if (isset($_SESSION['position']) && ($_SESSION['positon'] == 1)){
+if (isset($_SESSION['position']) && ($_SESSION['positon'] == 1)) {
+    include("./mvc/model/connect.php");
     include("./mvc/view/header.php");
-    if (isset($_GET['act'])){
-
+    include("mvc/model/category_func.php");
+    if (isset($_GET['act'])) {
+        switch ($_GET['act']) {
+            case 'logout':
+                //nếu act = logout tiến hành đăng xuất và trở về form login
+                unset($_SESSION['position']);
+                header("location: ./page/login.php");
+                break;
+            case 'home':
+                include("./mvc/view/home.php");
+                break;
+            case 'category_manage':
+                include('./mvc/view/category.php');
+                break;
+            case 'delete_category':
+                if (isset($_GET['id'])){
+                    $id = $_GET['id'];
+                    delete_category($conn, $id);
+                }
+                include('./mvc/view/category.php');
+                break;
+            default:
+                break;
+        }
+    } else {
+        include("./mvc/view/home.php");
     }
-}else {
-    header ("location: ./page/login.php");
+    include("./mvc/view/footer.php");
+} else {
+    header("location: ./page/login.php");
 }
 
 ?>
